f8126c336e20665169fa4f387ad1906fa144afa5
Shobhit Shrivastava <shrivastavashobhit706@gmail.com>
2026-08-05 12:22:00 +0530

    Basic client setup completed

--- client/src/app/page.jsx
+++ client/src/app/page.jsx
@@ -1,5 +1,10 @@
 export default function Home() {
   return (
-    <div>Home</div>
-  )
+    <main className="min-h-screen flex flex-col items-center justify-center gap-2">
+      <h1 className="text-5xl">Synthra AI</h1>
+      <p className="text-2xl">
+        Welcome to Synthra AI - Your AI Powered Workspace
+      </p>
+    </main>
+  );
 }
