1ef9babbb90c43a49ed5fbfe3193aec304786abe
Shobhit Shrivastava <shrivastavashobhit706@gmail.com>
2026-08-12 23:16:41 +0530

    Client: font setup

--- client/src/app/page.jsx
+++ client/src/app/page.jsx
@@ -1,9 +1,9 @@
 export default function Home() {
   return (
     <main className="min-h-screen flex flex-col items-center justify-center gap-2">
-      <h1 className="text-5xl">Synthra AI</h1>
-      <p className="text-2xl">
-        Welcome to Synthra AI - Your AI Powered Workspace
+      <h1 className="text-5xl font-[Cabinet]">Cognify</h1>
+      <p className="text-2xl font-[Satoshi]">
+        Welcome to Cognify - Your AI Powered Workspace
       </p>
     </main>
   );
