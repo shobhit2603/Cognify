9199fc1a92284b63863c87782147452a735f670c
Shashank Lakhera <lakherashashank70@gmail.com>
2026-08-18 10:46:21 +0530

    fix(chat): unwrap params Promise in dynamic route for Next 15 compat

--- client/src/app/chat/[id]/page.jsx
+++ client/src/app/chat/[id]/page.jsx
@@ -1,8 +1,8 @@
 "use client";
-import React from "react";
+import React, { use } from "react";
 import ChatCanvas from "../../../features/chat/components/ChatCanvas";
 
 export default function ActiveChatPage({ params }) {
-  const { id } = params;
+  const { id } = use(params);
   return <ChatCanvas chatId={id} />;
 }
